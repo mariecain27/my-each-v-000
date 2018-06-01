178d8b8c1ee63ae1e766269213b53fe4b90ce370
Marie Cain <mariecain27@gmail.com>
2018-06-01 02:50:41 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,18 @@
+#def my_each(array)
+#  array.each do |number|
+#    puts number
+#  end
+# end
+
 def my_each(array)
-  array.each do |number|
-    puts number
+  if block_given?
+    counter = 0
+    while counter < array.length
+      yield(array[counter])
+      counter += 1
+    end
+  array
+  else
+    puts "Hey! No block was given!"
   end
 end
